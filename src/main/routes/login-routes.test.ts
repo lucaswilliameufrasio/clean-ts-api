2b5e46cf903c33f5c826dcf2adfad5d8a5142521
Lucas William <lucaswilliameufrasio@gmail.com>
2020-08-26 06:01:10 -0300

    test: login route returns 401 when invalid credentails are provided

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -51,5 +51,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 when invalid credentials are provided', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'lucaswilliameufrasio@gmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
