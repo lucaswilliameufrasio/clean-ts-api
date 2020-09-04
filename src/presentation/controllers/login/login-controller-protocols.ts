98eb71a8f47fb6d6ffbb6e5f4ac97cd68056a4d6
Lucas William <lucaswilliameufrasio@gmail.com>
2020-09-04 04:32:55 -0300

    feat: ensure SignUpController calls Authentication with correct values

--- src/presentation/controllers/login/login-controller-protocols.ts
+++ src/presentation/controllers/login/login-controller-protocols.ts
@@ -1,3 +1,3 @@
 export * from '../../protocols/index'
-export * from '../../../domain/usecases/authentication'
 export * from '../../protocols/validation'
+export * from '../../../domain/usecases/authentication'
