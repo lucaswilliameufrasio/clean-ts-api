5547cee237ea64eeea58ead211a3121cc74ae37b
Lucas William <lucaswilliameufrasio@gmail.com>
2020-09-08 03:47:09 -0300

    test: ensure LoadSurveysController returns 204 if there is no content

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -1,6 +1,6 @@
 import { LoadSurveys, SurveyModel } from './load-surveys-controller-protocols'
 import { LoadSurveysController } from './load-surveys-controller'
-import { ok, serverError } from '../../../helpers/http/http-helper'
+import { ok, serverError, noContent } from '../../../helpers/http/http-helper'
 import MockDate from 'mockdate'
 
 const makeFakeSurveys = (): SurveyModel[] => {
@@ -69,6 +69,15 @@ describe('LoadSurveys Controller', () => {
     const httpResponse = await sut.handle({})
 
     expect(httpResponse).toEqual(ok(makeFakeSurveys()))
+  })
+
+  test('Should return 204 if LoadSurveys return empty', async () => {
+    const { sut, loadSurveysStub } = makeSut()
+    jest.spyOn(loadSurveysStub, 'load').mockReturnValueOnce(new Promise((resolve) => resolve([])))
+
+    const httpResponse = await sut.handle({})
+
+    expect(httpResponse).toEqual(noContent())
   })
 
   test('Should return 500 if LoadSurveys throws', async () => {
