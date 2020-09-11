41aba146625f185075ecf238b1a3a44c38ab87ba
Lucas William <lucaswilliameufrasio@gmail.com>
2020-09-11 01:30:14 -0300

    test: ensure DbSaveSurveyResult returns a SurveyResult on success

--- src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -66,4 +66,12 @@ describe('DbSaveSurveyResult UseCase', () => {
 
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return a list of SurveyResult on success', async () => {
+    const { sut } = makeSut()
+
+    const surveyResult = await sut.save(makeFakeSurveyResultData())
+
+    expect(surveyResult).toEqual(makeFakeSurveyResult())
+  })
 })
