b771b3b031976eb8ab36fe2c3f2bd0ffed367ac8
Lucas William <lucaswilliameufrasio@gmail.com>
2020-09-14 03:19:00 -0300

    test: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct values

---
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -0,0 +1,21 @@
+import { DbLoadSurveyResult } from './db-load-survey-result'
+import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
+import { mockSurveyResultModel } from '@/domain/test'
+import { SurveyResultModel } from '@/domain/models/survey-result'
+
+describe('DbLoadSurveyResult UseCase', () => {
+  test('Should call LoadSurveyResultRepository with correct values', async () => {
+    class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+      async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+        return mockSurveyResultModel()
+      }
+    }
+
+    const loadSurveyResultRepositoryStub = new LoadSurveyResultRepositoryStub()
+    const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+    const loadBySurveySpyId = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
+    await sut.load('any_survey_id')
+
+    expect(loadBySurveySpyId).toHaveBeenCalledWith('any_survey_id')
+  })
+})
