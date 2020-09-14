f7f4b8f85b9cf4c4aff3af4e14684b958969fd42
Lucas William <lucaswilliameufrasio@gmail.com>
2020-09-14 05:53:04 -0300

    test: ensure LoadSurveyResultController returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -77,12 +77,32 @@ describe('Survey Routes', () => {
         .expect(200)
     })
   })
-})
 
-describe('GET /surveys/:surveyId/results', () => {
-  test('Should return 403 on save survey result without accessToken', async () => {
-    await request(app)
-      .get('/api/surveys/any_id/results')
-      .expect(403)
+  describe('GET /surveys/:surveyId/results', () => {
+    test('Should return 403 on save survey result without accessToken', async () => {
+      await request(app)
+        .get('/api/surveys/any_id/results')
+        .expect(403)
+    })
+
+    test('Should return 200 on load survey result with accessToken', async () => {
+      const accessToken = await makeAccessToken()
+      const result = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+      const surveyId: string = result.ops[0]._id
+
+      await request(app)
+        .get(`/api/surveys/${surveyId}/results`)
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
   })
 })
