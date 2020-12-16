1b87f6065a31dd8c193e807e13a21b6e86aeb88c
Lucas William <lucaswilliameufrasio@gmail.com>
2020-12-15 22:44:49 -0300

    refactor: move code to a helper method

--- src/data/usecases/db-load-survey-result.ts
+++ src/data/usecases/db-load-survey-result.ts
@@ -1,29 +1,34 @@
 import { LoadSurveyResult } from '@/domain/usecases'
-import { SurveyResultModel } from '@/domain/models'
+import { SurveyModel, SurveyResultModel } from '@/domain/models'
 import { LoadSurveyResultRepository, LoadSurveyByIdRepository } from '@/data/protocols'
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
-  constructor (
+  constructor(
     private readonly loadSurveyResultRepository: LoadSurveyResultRepository,
     private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository
-  ) {}
+  ) { }
 
-  async load (surveyId: string, accountId: string): Promise<SurveyResultModel> {
+  async load(surveyId: string, accountId: string): Promise<SurveyResultModel> {
     let surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId, accountId)
     if (!surveyResult) {
       const survey = await this.loadSurveyByIdRepository.loadById(surveyId)
-      surveyResult = {
-        surveyId: survey.id,
-        question: survey.question,
-        date: survey.date,
-        answers: survey.answers.map(answer => Object.assign({}, answer, {
-          count: 0,
-          percent: 0,
-          isCurrentAccountAnswer: false
-        }))
-      }
+      surveyResult = this.makeEmptyResult(survey)
     }
 
     return surveyResult
+  }
+
+  private makeEmptyResult(survey: SurveyModel): SurveyResultModel {
+    return {
+      surveyId: survey.id,
+      question: survey.question,
+      date: survey.date,
+      answers: survey.answers.map(answer => ({
+        ...answer,
+        count: 0,
+        percent: 0,
+        isCurrentAccountAnswer: false
+      }))
+    }
   }
 }
