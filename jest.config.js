49fd3ccf713252088f7c34608961591394b28ad8
Lucas William <lucaswilliameufrasio@gmail.com>
2021-04-22 00:40:05 -0300

    chore: change jest config to ignore index files inside src folder

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   roots: ['<rootDir>/tests', '<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts'
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
